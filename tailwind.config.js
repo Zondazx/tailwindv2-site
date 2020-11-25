12bf304f7fb255192a06f9a469044fe8072b5dec
Zondazx <mzxgd16@gmail.com>
2020-11-25 19:56:45 -0400

    custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': '#1992d4',
+      },
+      spacing: {
+        '72': '18rem',
+      }
+    },
   },
   variants: {
     extend: {
